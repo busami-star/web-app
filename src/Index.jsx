7409b1172bb0092c68a991827f05d2096b89f48b
busami <157420366+busami@users.noreply.github.com>
2024-11-23 03:03:17 +0300

    initial commit

---
+++ src/Index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Index() {
+  return (
+    <>
+    <main>
+    <h2>Fun facts about React!</h2>
+      <ul>
+        <li>Was first release in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+      
+      
+    </>
+    
+  );
+}
+
+export default Index;
